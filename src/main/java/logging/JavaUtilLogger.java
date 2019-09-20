c02cb0d88baefc67d63d78586a8847d8b2756218
iourilitv <iourilitv1965@gmail.com>
2019-09-20 12:17:25 +0300

    Added logging by JUL for learning.

--- src/main/java/logging/JavaUtilLogger.java
+++ src/main/java/logging/JavaUtilLogger.java
@@ -1,12 +1,74 @@
 package logging;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
+/**
+ * Логгирование с помощью встроенного java логера.
+ * Источник: https://www.youtube.com/watch?v=e1Balqbin84
+ *
+ */
 public class JavaUtilLogger {
     private static final Logger logger = Logger.getLogger(JavaUtilLogger.class.getName());
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        //применить конфиуграционный файл
+        //LogManager.getLogManager().readConfiguration();
+        //TODO без него тоже выводится наш конфигуратор
+        // достаточно к конфигурации прописать путь к своему конфигуратору в VM option
+        //Например:
+        //-Djava.util.logging.config.file=D:\GeekBrains\_Mentoring\Mentoring\src\main\java\logging\logTest.properties
+
         //выводим путь и название конфигурационного файла для логирования проекта
         System.out.println(System.getProperty("java.util.logging.config.file"));
+        //result
+        //D:\GeekBrains\_Mentoring\Mentoring\src\main\java\logging\logTest.properties
+
+        //отключаем хандлер по умолчанию, чтобы не выводить в консоль!
+        //logger.setUseParentHandlers(false);
+        //С этой командой перестал выводить все логи
+
+        System.out.println("***Levels list***");
+        logger.log(Level.ALL, "ALL Level: " + Level.ALL.intValue());
+        //ALL: ALL Level: -2147483648 [Пт сен 20 12:13:15 MSK 2019]
+
+        //System.out.println(Level.FINEST.getName() + " Level: " + Level.FINEST.intValue());
+        //FINEST Level: 300
+        logger.log(Level.FINEST, "FINEST Level: " + Level.FINEST.intValue());
+        //Не отобразится пока уровень выставлен INFO
+
+        //System.out.println(Level.FINER.getName() + " Level: " + Level.FINER.intValue());
+        //FINER Level: 400
+        logger.log(Level.FINER, "FINER Level: " + Level.FINER.intValue());
+        //Не отобразится пока уровень выставлен INFO
+
+        //System.out.println(Level.FINE.getName() + " Level: " + Level.FINE.intValue());
+        //FINE Level: 500
+        logger.log(Level.FINE, "FINE Level: " + Level.FINE.intValue());
+        //Не отобразится пока уровень выставлен INFO
+
+        //System.out.println(Level.CONFIG.getName() + " Level: " + Level.CONFIG.intValue());
+        //CONFIG Level: 700
+        logger.log(Level.CONFIG, "CONFIG Level: " + Level.CONFIG.intValue());
+        //Не отобразится пока уровень выставлен INFO
+
+        logger.log(Level.INFO, "INFO Level: " + Level.INFO.intValue());
+        //сен 19, 2019 1:50:18 PM logging.JavaUtilLogger main
+        //INFO: INFO Level: 800
+
+        logger.log(Level.WARNING, "WARNING Level: " + Level.WARNING.intValue(), new Throwable());
+        //сен 19, 2019 1:50:18 PM logging.JavaUtilLogger main
+        //WARNING: WARNING Level: 900
+        //java.lang.Throwable
+        //	at logging.JavaUtilLogger.main(JavaUtilLogger.java:49)
+
+        logger.log(Level.SEVERE, "SEVERE Level: " + Level.SEVERE.intValue());
+        //сен 19, 2019 1:59:41 PM logging.JavaUtilLogger main
+        //SEVERE: SEVERE Level: 1000
+
+        logger.log(Level.OFF, "OFF Level: " + Level.OFF.intValue());
+        //OFF: OFF Level: 2147483647 [Пт сен 20 12:13:15 MSK 2019]
     }
 }
---
+++ src/main/java/logging/LogHandlers.java
@@ -0,0 +1,73 @@
+package logging;
+
+import java.io.IOException;
+import java.util.logging.*;
+import java.util.regex.Pattern;
+
+/**
+ * Логгирование с помощью встроенного java логера.
+ * Источник: https://www.youtube.com/watch?v=e1Balqbin84
+ *
+ */
+public class LogHandlers {
+    private static final Logger logger = Logger.getLogger(LogHandlers.class.getName());
+
+    public static void main(String[] args) throws IOException {
+
+        //хандлер для консоли
+        Handler consoleHandler = new ConsoleHandler();
+        //установим фильтр логов
+        consoleHandler.setFilter(new MyFilter());
+        //установим форматера логов
+        consoleHandler.setFormatter(new MyFormatter());
+
+        //хандлер для файла
+        //в параметр можно передать паттерн, описав напрямую или в виде объекта паттерна
+        //Pattern pattern = Pattern("", 1); забыл синтаксис
+        //%h - указывает на домашнюю директорию, там может лежать файл лога;
+        //%u - sequence(последовательность)
+        Handler fileHandler = new FileHandler("%h/MyJavaLog.log");
+
+        //хендлер для какого-то потока(чтения или записи, например)
+        //Handler streamHandler = new StreamHandler();
+        //хандлер соединения
+        //Handler socketHandler = new SocketHandler();
+        //хендлер для вывода в память
+        //Handler memoryHandler = new MemoryHandler();
+
+        //устанавливаем хендлер для логера
+        //отключаем хандлер по умолчанию, чтобы не выводить в консоль!
+        logger.setUseParentHandlers(false);
+        //устанавливаем логеру хендлер - пишем в файл
+        logger.addHandler(fileHandler);
+        //устанавливаем логеру свой хендлер для консоли
+        logger.addHandler(consoleHandler);
+
+        logger.info("INFO");
+        //сен 19, 2019 2:25:21 PM logging.LogHandlers main
+        //INFO: INFO
+
+        logger.info("Info max");
+    }
+
+    //установим фильтр логов
+    static class MyFilter implements Filter{
+
+        @Override
+        public boolean isLoggable(LogRecord record) {
+            //выводим только логи, оканчивающиеся на max
+            return record.getMessage().endsWith("max");
+        }
+    }
+
+    //изменим формат представления лога
+    static class MyFormatter extends Formatter{
+
+        @Override
+        public String format(LogRecord record) {
+            return record.getLevel() + ": " + record.getMessage();
+        }
+    }
+
+}
+
